0ae3c1a4980d53b84184c72afed159695464f3fe
Peter Ingles <peter.ingles@gmail.com>
2023-08-20 15:26:05 -0500

    Add conditional rendering for Header

--- src/components/App/Header.jsx
+++ src/components/App/Header.jsx
@@ -3,13 +3,35 @@ import { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 export const Header = () => {
-    const surveyAnswers = useSelector(store => store.surveyAnswers)
-    
- return (<>
-    <header className='App-header'>
-      <h1 className='App-title'>Feedback!</h1>
-      <h4>Question {surveyAnswers.length + 1} of 4</h4>
-    </header>
-  </>
-);
- }
+    const surveyAnswers = useSelector(store => store.surveyAnswers);
+  
+    if (surveyAnswers.length < 4) {
+      return (
+        <>
+          <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>Question {surveyAnswers.length + 1} of 4</h4>
+          </header>
+        </>
+      );
+    } if(surveyAnswers.length == 4) {
+      return (
+        <>
+          <header className='App-header'>
+            <h1 className='App-title'>Feedback!</h1>
+            <h4>REVIEW YOUR ANSWERS</h4>
+          </header>
+        </>
+      );
+    }else {
+        return (
+          <>
+            <header className='App-header'>
+              <h1 className='App-title'>Feedback!</h1>
+              <h4>THANK YOU FOR YOUR FEEDBACK!</h4>
+            </header>
+          </>
+        );
+      }
+  };
+  
